29fe22338b573c3b2319998e2a798b0aa88e757d
Ben Edwards <hello@benedfit.com>
2021-09-01 09:40:33 +0100

    feat(parameters): sets default controls matchers

--- src/preview.js
+++ src/preview.js
@@ -2,6 +2,12 @@ import { DocsPage } from './components'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  },
   docs: {
     page: DocsPage
   },
