0ddc57e64c3e65847b402958eaec87d2854849aa
Ben Edwards <hello@benedfit.com>
2023-09-01 13:03:49 +0100

    feat(storybook-addon-transpile-modules): initial release

---
+++ packages/storybook-addon-transpile-modules/src/__tests__/index.spec.js
@@ -0,0 +1,40 @@
+import * as addon from '..'
+
+describe('addon-transpile-modules', () => {
+  it('should return webpackFinal', () => {
+    expect(typeof addon.webpackFinal).toEqual('function')
+  })
+
+  it("should do nothing if 'transpileModules' is not set", () => {
+    const webpackConfig = addon.webpackFinal()
+
+    expect(webpackConfig).toEqual({})
+  })
+
+  it('should do nothing existing webpack rule is not found', () => {
+    const baseConfig = { module: { rules: [] } }
+    const { rules: baseConfigRules } = baseConfig.module
+    const webpackConfig = addon.webpackFinal(baseConfig, {
+      transpileModules: ['foo', 'bar']
+    })
+    const { rules } = webpackConfig.module
+
+    expect(rules.length).toEqual(baseConfigRules.length)
+  })
+
+  it('should override existing webpack rule', () => {
+    const baseConfig = { module: { rules: [{ exclude: ['node_modules'] }] } }
+    const { rules: baseConfigRules } = baseConfig.module
+    const existingRule = baseConfigRules.findIndex(rule =>
+      rule.exclude.toString().includes('node_modules')
+    )
+    const webpackConfig = addon.webpackFinal(baseConfig, {
+      transpileModules: ['foo', 'bar']
+    })
+    const { rules } = webpackConfig.module
+
+    expect(rules[existingRule].exclude).toEqual(
+      /node_modules\/(?!(foo|bar)\/).*/
+    )
+  })
+})
---
+++ packages/storybook-addon-transpile-modules/src/index.js
@@ -0,0 +1,22 @@
+/** @typedef { import('@storybook/react-webpack5').StorybookConfig } StorybookConfig */
+
+/** @type { StorybookConfig['webpackFinal'] } */
+export const webpackFinal = (config = {}, options = {}) => {
+  const { transpileModules = [] } = options
+
+  if (transpileModules.length) {
+    // Find existing rule that handles JSX
+    const existing = config.module.rules?.find(rule =>
+      rule.exclude?.toString().includes('node_modules')
+    )
+
+    if (existing) {
+      // Tell existing rule to not exclude modules that need transpiling
+      existing.exclude = new RegExp(
+        `node_modules/(?!(${transpileModules.join('|')})/).*`
+      )
+    }
+  }
+
+  return config
+}
