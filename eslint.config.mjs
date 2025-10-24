956ed39fe962ab918a8e1fc14fa48f5d31d89e89
Ben Edwards <hello@benedfit.com>
2025-10-24 13:52:11 +0100

    chore(eslint): use standard tsconfig (#2250)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,4 @@
 import config from '@newhighsco/eslint-config'
 import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
-  { extends: [config] },
-  {
-    files: ['**/*.ts?(x)'],
-    languageOptions: { parserOptions: { project: './tsconfig.eslint.json' } },
-    rules: { 'tsc/config': ['error', { configFile: './tsconfig.eslint.json' }] }
-  }
-])
+export default defineConfig([{ extends: [config] }])
