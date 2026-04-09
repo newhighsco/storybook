d00fdb1eab6b540de7bf84604a8494d98a04dfaf
Ben Edwards <hello@benedfit.com>
2026-04-09 13:25:48 +0100

    chore(deps): migrate to `tsdown` (#2519)
    * chore(deps): migrate to `tsdown`
    
    * chore: revert

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,12 @@
 import { readFile } from 'node:fs/promises'
 import { posix } from 'node:path'
 
-import { defineConfig, type Options } from 'tsup'
+import { defineConfig, type UserConfig } from 'tsdown'
 
 export default defineConfig(async ({ watch }) => {
   const srcDir = './src'
   const targets: Partial<
-    Record<NonNullable<Options['platform']>, Options['target']>
+    Record<NonNullable<UserConfig['platform']>, UserConfig['target']>
   > = { browser: 'esnext', node: 'node20.19' }
   const { exports = {} } = await readFile('./package.json', 'utf-8').then(
     JSON.parse
@@ -16,17 +16,19 @@ export default defineConfig(async ({ watch }) => {
 
   const entry = (
     fileNames: string | string[],
-    { platform = 'browser', ...options }: Options = {}
-  ): Options => {
+    { dts = false, platform = 'browser', ...options }: UserConfig = {}
+  ): UserConfig => {
     if (!Array.isArray(fileNames)) fileNames = [fileNames]
 
     return {
       clean: !watch,
+      deps: {
+        neverBundle: ['@storybook/icons', 'react', 'react-dom', 'webpack']
+      },
+      dts,
       entry: fileNames.map(fileName => posix.join(srcDir, fileName)),
-      external: ['react', 'react-dom', '@storybook/icons'],
       format: ['esm'],
       platform,
-      splitting: true,
       target: targets[platform],
       treeshake: true,
       ...options
